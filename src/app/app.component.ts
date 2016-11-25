351d0e57b56786af90e73b200ff1bcc384927acd
DevWurm <devwurm@devwurm.net>
2016-11-25 10:03:05 +0100

    Adding stormpath registering into frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Account, Stormpath } from 'angular-stormpath';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public title = 'app works!';
+  private user$: Observable<Account | boolean>;
+  private loggedIn$: Observable<boolean>;
+  private login: boolean;
+  private register: boolean;
+
+  constructor(public stormpath: Stormpath) {
+  }
+
+  ngOnInit() {
+    this.login = true;
+    this.register = false;
+    this.user$ = this.stormpath.user$;
+    this.loggedIn$ = this.user$.map(user => !!user);
+  }
+
+  showLogin() {
+    this.login = !(this.register = false);
+  }
+
+  showRegister() {
+    this.register = !(this.login = false);
+  }
+
+  logout() {
+    this.stormpath.logout();
+  }
 }
